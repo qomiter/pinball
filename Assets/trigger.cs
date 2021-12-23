20223939514d272820c7f1fa5801c486683ad3de
qomiter <qomiter@gmail.com>
2021-12-23 15:19:25 -0500

    Added Save High Score

--- Assets/trigger.cs
+++ Assets/trigger.cs
@@ -7,6 +7,8 @@ public class trigger : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        PlayerPrefs.SetInt("highscore", scoretracker.highScore);
+        PlayerPrefs.Save();
         SceneManager.LoadScene(0);
     }
 }
